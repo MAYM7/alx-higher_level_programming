ccfb0fb47b9c332ffd26bf609a369ed3f433ed07
MAYM7 <mmkoko8@gmail.com>
2024-01-16 14:33:35 -0800

    fix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 for i in range(0, 10):
-    for l in range(digit1 + 1, 10):
+    for l in range(i + 1, 10):
         if i == 8 and l == 9:
-            print("{}{}".format(digit1, digit2))
+            print("{}{}".format(i, l))
         else:
-            print("{}{}".format(digit1, digit2), end=", ")
+            print("{}{}".format(i, l), end=", ")
