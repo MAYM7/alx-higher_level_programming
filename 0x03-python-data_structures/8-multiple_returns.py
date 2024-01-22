f6472aedf442d6dbf7e8ce2cebb681ac30a8c25a
MAYM7 <mmkoko8@gmail.com>
2024-01-21 19:47:58 -0800

    done

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,7 +3,7 @@
 def multiple_returns(sentence):
     leng = len(sentence)
     first = sentence[0]
-    if leng == 0:
+    if sentence == "":
         return (0, None)
     else:
         return (leng, first)
